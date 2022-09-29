7423249c1af0de5f1b667ea7d1eb30f3afcc496c
AnjaliRadhe <111715453+AnjaliRadhe@users.noreply.github.com>
2022-09-29 20:31:16 +1000

    arraylit

---
+++ Arraylist_eg/src/Smaple.java
@@ -0,0 +1,17 @@
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Smaple {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		List l= new ArrayList();
+		l.add("hello");
+		l.add(20);
+		l.add(true);
+		l.add("null");
+		System.out.println(l);
+	}
+
+}
